a8596d574734262a04f134b0786de411ad2a11d2
Mariano Carballal <marianocarballal@gmail.com>
2013-09-12 01:30:00 -0300

    Some code refactor

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect
   , path = require('path')
   , basedir = path.join(__dirname, '..')
-  , Redmine = require(path.join(basedir, 'redmine.js'))
+  , Redmine = require(path.join(basedir, 'lib/redmine.js'))
   , assert = require('assert')
   , should = require('chai').should()
 
@@ -10,10 +10,18 @@ assert.ok('REDMINE_HOST' in process.env);
 
 var redmine = new Redmine(process.env.REDMINE_HOST,process.env.REDMINE_APIKEY)
 
-describe('#call with limit', function () {
+describe('#call small with limit', function () {
     it('should return 1 user', function () {
         redmine.api('users', {limit:1}, function(err, res){
           res.should.have.length(1)
+        })        
+    });
+});
+
+describe('#call with huge limit', function () {
+    it('should return 300 user', function () {
+        redmine.api('users', {limit:300}, function(err, res){
+          res.should.have.length(1000)
         })        
     });
 });
