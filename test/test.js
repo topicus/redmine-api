65f9586972665ca20c18cdfd1a707bf9807cae8a
Mariano Carballal <marianocarballal@gmail.com>
2013-09-18 15:18:22 -0300

    Adding update on polifill

--- test/test.js
+++ test/test.js
@@ -11,17 +11,21 @@ assert.ok('REDMINE_HOST' in process.env);
 var redmine = new Redmine(process.env.REDMINE_HOST,process.env.REDMINE_APIKEY)
 
 describe('#call small with limit', function () {
-    it('should return 1 user', function () {
-        redmine.api('users', {limit:1}, function(err, res){
-          res.should.have.length(1)
+    it('should return 1 user', function (done) {
+        redmine.api('users', {numRows:10}, function(err, res){
+          if (err) return done(err);
+          res.should.have.length(10);
+          done();          
         })        
     });
 });
 
 describe('#call with huge limit', function () {
-    it('should return 300 user', function () {
-        redmine.api('users', {limit:300}, function(err, res){
-          res.should.have.length(1000)
+    it('should return 200 user', function (done) {
+        redmine.api('users', {numRows:200}, function(err, res){
+          if (err) return done(err);
+          res.should.have.length(200);
+          done();
         })        
     });
 });
